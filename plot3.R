ce0ddda4c35a2067586a381c40a2746959117027
Joanap08 <joana.pinto@booking.com>
2016-12-12 00:57:46 +0000

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -5,12 +5,11 @@ NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
 #Select Baltimore only
-
 mary<-NEI[NEI$fips=="24510",]
 
-#Need to factorize the years so we can use them for the graph
-mary$year <- factor(mary$year, levels=c('1999', '2002', '2005', '2008'))
+#agrregate so we can have the years and types as factors.
 
+balt.agg <- aggregate(mary[c("Emissions")], list(type = mary$type, year = mary$year), sum)
 
-#Initial call to qplot
-qplot(year,Emissions, data=mary, facets = .~type, geom="line", ylim=c(0,500))
+#qplot
+qplot(year, Emissions, data = balt.agg, color = type, geom= "line")+ ggtitle("Total PM2.5 Emissions in Baltimore by Source ") + xlab("Year") + ylab("PM2.5 Emissions(tons)")  
