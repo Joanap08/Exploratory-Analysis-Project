471d2fd4baba8c874c1d6514ef814cc1f1d812d3
Joanap08 <joana.pinto@booking.com>
2016-12-18 09:26:13 +0000

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -20,5 +20,5 @@ both<-NEI[NEI$SCC %in% extract$SCC,]
 #try to summarize variables to have the total
 sources<-summarise(group_by(both,year), Emissions=sum(Emissions))
 #plot
-ggplot(data=sources,aes(x=factor(year), y=Emissions/1000)+ geom_bar(aes(fill=cond2),stat="identity",position = 'dodge')))+ xlab("Period (years)")+ylab("Total PM2.5 Emissions (kilotons)")+ggtitle("Coal Emissions over a certain period in USA")
+ggplot(data=sources,aes(x=factor(year), y=Emissions/1000)+ geom_bar(aes(fill=year),stat="identity",position = 'dodge')))+ xlab("Period (years)")+ylab("Total PM2.5 Emissions (kilotons)")+ggtitle("Coal Emissions over a certain period in USA")
 
