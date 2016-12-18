0051a0d7670342e57ada40ba0cf728a36cdd70bc
Joanap08 <joana.pinto@booking.com>
2016-12-18 22:05:40 +0000

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -11,7 +11,7 @@ library(ggplot2)
 
 #Across the United States, how have emissions from coal combustion-related sources changed from 1999–2008?
 
-#First select from SCC all the coal related sources
+#First select from SCC all the combustion coal related sources
 coal<- grepl("Fuel Comb.*Coal", SCC$EI.Sector)
 extract<-SCC[coal,]
 #merge both datasets based on SCC
@@ -20,5 +20,12 @@ both<-NEI[NEI$SCC %in% extract$SCC,]
 #try to summarize variables to have the total
 sources<-summarise(group_by(both,year), Emissions=sum(Emissions))
 #plot
-ggplot(data=sources,aes(x=factor(year), y=Emissions/1000)+ geom_bar(aes(fill=year),stat="identity",position = 'dodge')))+ xlab("Period (years)")+ylab("Total PM2.5 Emissions (kilotons)")+ggtitle("Coal Emissions over a certain period in USA")
-
+png(filename = "plot4.png")
+ggplot(sources,aes(x=factor(year), y=Emissions/1000, group=1, label=round(Emissions/1000,2)))+
+  geom_line(color="light blue",size= 1.5)+
+  geom_point(shape=23, fill="blue",size=3)+
+  geom_text(aes(fill = year),colour = "black", fontface = "italic",vjust = -0.6, nudge_y = 0.5)+
+  xlab("Year")+
+  ylab("PM2.5 Coal combustion-sources emissions(kilotons)") +
+  ggtitle("Emissions from coal combustion-related sources in USA")
+dev.off()
